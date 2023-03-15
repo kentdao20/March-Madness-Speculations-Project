45237572ed5305cdad5e5b97d608e08f4bb68733
joshlerr <eyasuyenesew20@augustana.edu>
2023-03-14 19:30:00 -0500

    Update R March madness.R

--- R March madness.R
+++ R March madness.R
@@ -1,5 +1,15 @@
 #Group: Kent, Josh, Sally
-
 library(tidyverse)
-library(
-)
+library(tidytext)
+library(textdata)
+library(janeaustenr)
+library(dplyr)
+library(stringr)
+library(reshape2)
+library(shiny)
+rm(list=ls())
+
+setwd("C:/Data 332 project 2/March-Madness-Speculations-Project")
+
+df<-read.csv("2023 Tournament Data.csv")%>%
+  select(TEAM, KENPOM ADJUSTED EFFICIENCY)
