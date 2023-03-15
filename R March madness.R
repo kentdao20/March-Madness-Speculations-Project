c63fdcdca35efb6d343d51634908e2da254e6ad1
joshlerr <eyasuyenesew20@augustana.edu>
2023-03-14 19:42:34 -0500

    Update R March madness.R

--- R March madness.R
+++ R March madness.R
@@ -15,3 +15,16 @@ setwd("~/Desktop/DATA 332/March-Madness-Speculations-Project")
 
 df<-read.csv("2023 Tournament Data.csv")%>%
   select(TEAM, KENPOM.ADJUSTED.EFFICIENCY)
+
+colnames
+df<-read.csv("2023 Tournament Data.csv")
+colnames(6)<-"K efficiency"
+colnames(7)<-"K offense"
+colnames(8)<-"K defense"
+colnames(9)<-"K tempo"
+colnames(10)<-"B efficiency"
+colnames(11)<-"B offense"
+colnames(12)<-"B defense"
+colnames(13)<-"B tempo"
+
+#select(TEAM, KENPOM.ADJUSTED.EFFICIENCY,KENPOM.ADJUSTED.OFFENSE, KENPOM.ADJUSTED.DEFENSE, KENPOM.ADJUSTED.TEMPO, BAR.ADJUSTED.EFFICIENCY)
