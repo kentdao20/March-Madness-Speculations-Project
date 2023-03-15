dd392274582f685071fc43be50ef8b7a69c85baa
joshlerr <eyasuyenesew20@augustana.edu>
2023-03-14 19:56:28 -0500

    Update R March madness.R

--- R March madness.R
+++ R March madness.R
@@ -9,25 +9,22 @@ library(reshape2)
 library(shiny)
 rm(list=ls())
 
-#setwd("C:/Data 332 project 2/March-Madness-Speculations-Project")
+setwd("C:/Data 332 project 2/March-Madness-Speculations-Project")
 #setwd("D:/DATA 332/Github Project 1/March-Madness-Speculations-Project")
-setwd("~/Desktop/DATA 332/March-Madness-Speculations-Project")
+#setwd("~/Desktop/DATA 332/March-Madness-Speculations-Project")
 
 #opened the csv file and proceeded in the data cleaning process
 
-df<-read.csv("2023 Tournament Data.csv")%>%
-  
-  select(TEAM, KENPOM.ADJUSTED.EFFICIENCY)
-
-colnames
 df<-read.csv("2023 Tournament Data.csv")
-colnames(6)<-"K efficiency"
-colnames(7)<-"K offense"
-colnames(8)<-"K defense"
-colnames(9)<-"K tempo"
-colnames(10)<-"B efficiency"
-colnames(11)<-"B offense"
-colnames(12)<-"B defense"
-colnames(13)<-"B tempo"
+colnames(df)[6]<-"K_efficiency"
+colnames(df)[7]<-"K_offense"
+colnames(df)[8]<-"K_defense"
+colnames(df)[9]<-"K_tempo"
+colnames(df)[10]<-"B_efficiency"
+colnames(df)[11]<-"B_offense"
+colnames(df)[12]<-"B_defense"
+colnames(df)[15]<-"B_tempo"
+#colnames(13)<-"B-tempo"
 
-#select(TEAM, KENPOM.ADJUSTED.EFFICIENCY,KENPOM.ADJUSTED.OFFENSE, KENPOM.ADJUSTED.DEFENSE, KENPOM.ADJUSTED.TEMPO, BAR.ADJUSTED.EFFICIENCY)
+df2<-df%>%
+  select(K_efficiency,K_offense,K_defense,K_tempo,B_efficiency,B_offense,B_defense)
