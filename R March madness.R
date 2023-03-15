ad4d6f78aa0f1f861627f4b9c1997d08ab80d459
sallyboutaleb <sallyboutaleb21@augustana.edu>
2023-03-14 19:35:48 -0500

    Update R March madness.R

--- R March madness.R
+++ R March madness.R
@@ -10,8 +10,8 @@ library(shiny)
 rm(list=ls())
 
 #setwd("C:/Data 332 project 2/March-Madness-Speculations-Project")
-setwd("D:/DATA 332/Github Project 1/March-Madness-Speculations-Project")
-
+#setwd("D:/DATA 332/Github Project 1/March-Madness-Speculations-Project")
+setwd("~/Desktop/DATA 332/March-Madness-Speculations-Project")
 
 df<-read.csv("2023 Tournament Data.csv")%>%
   select(TEAM, KENPOM.ADJUSTED.EFFICIENCY)
