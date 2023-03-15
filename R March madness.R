4122bf7d6c59cdf33ed76a045129c462aa9dae13
kienkcp <kentdaokcp@gmail.com>
2023-03-14 20:04:05 -0500

    Update R March madness.R

--- R March madness.R
+++ R March madness.R
@@ -9,8 +9,8 @@ library(reshape2)
 library(shiny)
 rm(list=ls())
 
-setwd("C:/Data 332 project 2/March-Madness-Speculations-Project")
-#setwd("D:/DATA 332/Github Project 1/March-Madness-Speculations-Project")
+#setwd("C:/Data 332 project 2/March-Madness-Speculations-Project")
+setwd("D:/DATA 332/Github Project 1/March-Madness-Speculations-Project")
 #setwd("~/Desktop/DATA 332/March-Madness-Speculations-Project")
 
 #opened the csv file and proceeded in the data cleaning process
