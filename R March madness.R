bf3ee90b1cf8e94e19b90754c49a48d24ef17750
joshlerr <eyasuyenesew20@augustana.edu>
2023-03-17 16:59:08 -0500

    Update R March madness.R

--- R March madness.R
+++ R March madness.R
@@ -29,8 +29,8 @@ colnames(df)[15]<-"B_tempo"
 df2<-df%>%
   select(TEAM, K_efficiency,K_offense,K_defense,K_tempo,B_efficiency,B_offense,B_defense,B_tempo)
 
-south_df <- df2%>%
-  dplyr::filter(TEAM == "Alabama")
+ 
+  
 df2$sum_efficiency<-as.numeric(df2$K_efficiency)+ as.numeric(df2$B_efficiency)
 df2$average_efficiency<-df2$sum_efficiency/2  
 df2$sum_offense<-as.numeric(df2$K_offense)+ as.numeric(df2$B_offense)
@@ -40,6 +40,15 @@ df2$average_defense<-df2$sum_defense/2
 df2$sum_tempo<-as.numeric(df2$K_tempo)+ as.numeric(df2$B_tempo)
 df2$average_tempo<-df2$sum_tempo/2
 df2$all_average<-(df2$average_efficiency+df2$average_offense+df2$average_defense+df2$average_tempo)/2
+
+south_df <- df2%>%
+  filter((TEAM == "Alabama") | (TEAM =="Texas A&M-Corpus Christi") | (TEAM == "Maryland") | (TEAM == "West Virginia") | (TEAM == "San Diego State") | 
+           (TEAM == "College of Charleston") | (TEAM == "Virginia") | (TEAM == "Furman") | (TEAM == "Creighton") | (TEAM == "North Carolina State") | 
+           (TEAM == "Baylor") | (TEAM =="UC Irvine") | (TEAM == "Missouri") | (TEAM == "Utah Valley") | (TEAM == "Arizona") | (TEAM == "Arizona State"))
+
+
+
+
 #comparing them by offense
 df3<-df2[order(df2$average_offense, decreasing = TRUE),]
 
