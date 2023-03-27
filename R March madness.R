159cc325137a87729d0458607d64d265bfdcbd59
sallyboutaleb <sallyboutaleb21@augustana.edu>
2023-03-26 20:35:46 -0500

    Update R March madness.R

--- R March madness.R
+++ R March madness.R
@@ -10,9 +10,9 @@ library(ggplot2)
 library(patchwork)
 rm(list=ls())
 
-setwd("C:/Data 332 project 2/March-Madness-Speculations-Project")
+#setwd("C:/Data 332 project 2/March-Madness-Speculations-Project")
 #setwd("D:/DATA 332/Github Project 1/March-Madness-Speculations-Project")
-#setwd("~/Desktop/DATA 332/March-Madness-Speculations-Project")
+setwd("~/Desktop/DATA 332/March-Madness-Speculations-Project")
 
 #opened the csv file and proceeded in the data cleaning process
 
